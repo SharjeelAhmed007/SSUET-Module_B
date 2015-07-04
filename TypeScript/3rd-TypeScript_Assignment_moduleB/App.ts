fe5a0cae38fd9f1377cc24ad4e58310b6268d6c0
SharjeelAhmed007 <sharjeelahmedt49@gmail.com>
2015-07-03 20:24:49 -0700

    typescript file

---
+++ TypeScript/3rd-TypeScript_Assignment_moduleB/App.ts
@@ -0,0 +1,13 @@
+function Save_rec()
+{
+	
+}
+function cleartxtBoxes():void {
+	(<HTMLInputElement>document.getElementById("stu_id")).value='';
+	(<HTMLInputElement>document.getElementById("stu_name")).value='';
+	(<HTMLInputElement>document.getElementById("stu_class")).value='';
+	(<HTMLInputElement>document.getElementById("DS")).value='';
+	(<HTMLInputElement>document.getElementById("DB")).value='';
+	(<HTMLInputElement>document.getElementById("Aut")).value='';
+	(<HTMLInputElement>document.getElementById("SD")).value='';
+}
