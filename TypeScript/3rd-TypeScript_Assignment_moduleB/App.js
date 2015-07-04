1bdebb646b01f22642667ce9d3179181fd5ffc3a
SharjeelAhmed007 <sharjeelahmedt49@gmail.com>
2015-07-03 20:24:24 -0700

    js file

---
+++ TypeScript/3rd-TypeScript_Assignment_moduleB/App.js
@@ -0,0 +1,11 @@
+function Save_rec() {
+}
+function cleartxtBoxes() {
+    document.getElementById("stu_id").value = '';
+    document.getElementById("stu_name").value = '';
+    document.getElementById("stu_class").value = '';
+    document.getElementById("DS").value = '';
+    document.getElementById("DB").value = '';
+    document.getElementById("Aut").value = '';
+    document.getElementById("SD").value = '';
+}
